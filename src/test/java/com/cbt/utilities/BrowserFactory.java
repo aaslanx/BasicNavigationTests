70a48172dbf508a2a0db792de646a5bd52960c63
aaslanx <aaslanx@gmail.com>
2019-10-27 23:44:59 -0500

    Firefox fixed

--- src/test/java/com/cbt/utilities/BrowserFactory.java
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class BrowserFactory {
@@ -31,6 +32,7 @@ public class BrowserFactory {
                 return new ChromeDriver();
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
         }
         return null;
     }
